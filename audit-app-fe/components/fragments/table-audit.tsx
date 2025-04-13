ffd506141d637bb9eb34d6c296397735e99c3239
pazcalll <yazeedarifin@gmail.com>
2025-04-14 03:57:05 +0700

    feat: update dialog

--- audit-app-fe/components/fragments/table-audit.tsx
+++ audit-app-fe/components/fragments/table-audit.tsx
@@ -14,12 +14,26 @@ import { TAudit } from "../types/audit";
 import { getAudits } from "../api/authenticated/admin/get-audits";
 import { toast } from "sonner";
 import { TMeta } from "../types/api-response";
+import moment from "moment";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "@radix-ui/react-label";
 
 export default function TableAudit() {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [lastPage, setLastPage] = useState<number>(2); // Replace with actual last page number
   const [audits, setAudits] = useState<TAudit[]>([]); // Replace with actual audit type
   const [selectedAudit, setSelectedAudit] = useState<TAudit | null>(null); // Replace with actual selected audit type
+
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchAudits = async () => {
@@ -45,7 +59,7 @@ export default function TableAudit() {
   }, [currentPage]);
 
   return (
-    <div>
+    <div className="max-w-[100vw] md:max-w-[100rem] overflow-auto">
       <Table className="border-[1px] border-gray-300 rounded-md">
         <TableHeader>
           <TableRow>
@@ -63,7 +77,10 @@ export default function TableAudit() {
           {audits.map((audit) => (
             <TableRow
               key={audit.id}
-              onClick={() => setSelectedAudit(audit)}
+              onClick={() => {
+                setSelectedAudit(audit);
+                setOpenDialog(true);
+              }}
               className="cursor-pointer hover:bg-gray-100"
             >
               <TableCell className="font-medium">
@@ -74,12 +91,25 @@ export default function TableAudit() {
               <TableCell>{audit.auditable_id}</TableCell>
               <TableCell>{audit.url}</TableCell>
               <TableCell>{audit.ip_address}</TableCell>
-              <TableCell>{audit.created_at}</TableCell>
-              <TableCell>{audit.updated_at}</TableCell>
+              <TableCell>
+                {moment
+                  .parseZone(audit.created_at)
+                  .format("YYYY-MM-DD HH:mm:ss")}
+              </TableCell>
+              <TableCell>
+                {moment
+                  .parseZone(audit.updated_at)
+                  .format("YYYY-MM-DD HH:mm:ss")}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <AuditDialog
+        audit={selectedAudit as TAudit}
+        open={openDialog}
+        setOpen={setOpenDialog}
+      />
       <div className="flex w-full justify-center items-center mt-4">
         <TablePagination
           currentPage={currentPage}
@@ -88,5 +118,75 @@ export default function TableAudit() {
         />
       </div>
     </div>
+  );
+}
+
+function AuditDialog({
+  audit,
+  open,
+  setOpen,
+}: {
+  audit: TAudit;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  if (!audit) return null; // Return null if audit is not available
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Audit Detail</DialogTitle>
+        </DialogHeader>
+        <div className="space-x-2">
+          <div className="w-sm my-1">
+            <Label className="text-sm font-medium">PIC</Label>
+            <Input
+              type="text"
+              value={(audit?.user as { name?: string }).name || ""}
+              readOnly
+              className="w-full"
+            />
+          </div>
+          <div className="w-sm my-1">
+            <Label className="text-sm font-medium">Auditable Type</Label>
+            <Input
+              type="text"
+              value={audit?.auditable_type || ""}
+              readOnly
+              className="w-full"
+            />
+          </div>
+          <div className="w-sm my-1">
+            <Label className="text-sm font-medium">Auditable ID</Label>
+            <Input
+              type="text"
+              value={audit?.auditable_id || ""}
+              readOnly
+              className="w-full"
+            />
+          </div>
+          <div className="w-sm my-1">
+            <Label className="text-sm font-medium">Old Data</Label>
+            <pre className="bg-gray-200 rounded-md overflow-auto">
+              {JSON.stringify(audit?.old_values, null, 2)}
+            </pre>
+          </div>
+          <div className="w-sm my-1">
+            <Label className="text-sm font-medium">New Data</Label>
+            <pre className="bg-gray-200 rounded-md overflow-auto">
+              {JSON.stringify(audit?.new_values, null, 2)}
+            </pre>
+          </div>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary" className="w-full">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
